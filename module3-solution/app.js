6c22de561a0706bbee40b25db7183df302e31a66
joanalima <joanaduartelima@gmail.com>
2018-09-02 20:02:30 +0200

    adding module3 assignment

---
+++ module3-solution/app.js
@@ -0,0 +1,88 @@
+(function () {
+  'use strict';
+
+  angular.module('NarrowItDownApp', [])
+  .controller('NarrowItDownController', NarrowItDownController)
+  .service('MenuSearchService', MenuSearchService)
+  .constant('ApiBasePath', 'https://davids-restaurant.herokuapp.com')
+  .directive('foundItems', FoundItemsDirective);
+
+  function FoundItemsDirective() {
+    var ddo = {
+      templateUrl: 'foundItems.html',
+      scope: {
+        items: '<',
+        onRemove: '&'
+      },
+      controller: NarrowItDownController,
+      controllerAs: 'menuCtrl',
+      bindToController: true
+    }
+
+    return ddo;
+  }
+
+  NarrowItDownController.$inject = ['MenuSearchService'];
+  function NarrowItDownController(MenuSearchService) {
+    var menuCtrl = this;
+
+    menuCtrl.searchTerm = '';
+    menuCtrl.foundItems = '';
+
+    menuCtrl.searchMenu = function() {
+
+      var promise = MenuSearchService.getMatchedMenuItems(menuCtrl.searchTerm.toLowerCase());
+      menuCtrl.message = '';
+
+      if(menuCtrl.searchTerm == '') {
+        menuCtrl.message = 'Nothing found';
+        menuCtrl.foundItems = '';
+      }
+
+      if( menuCtrl.searchTerm != '' ){
+        promise.then( function(foundItems) {
+          if( foundItems.length == 0 ){
+            menuCtrl.message = 'Nothing found';
+          } else {
+            menuCtrl.foundItems = foundItems;
+          }
+        });
+      }
+    };
+
+    menuCtrl.removeFromSearch = function(index) {
+      menuCtrl.foundItems.splice(index, 1);
+    }
+  }
+
+  MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+  function MenuSearchService($http, ApiBasePath) {
+    var service = this;
+
+    service.getMatchedMenuItems = function(searchTerm){
+      return $http({
+        method: 'GET',
+        url: (ApiBasePath + '/menu_items.json')
+      }).then( function (result) {
+
+        // process result and only keep items that match
+        var foundItems = [];
+        var data = result.data;
+
+        data.menu_items.forEach( function(menuItem) {
+          if ( menuItem.description.indexOf(searchTerm) != -1 ) {
+            foundItems.push({
+                name: menuItem.name,
+                short_name: menuItem.short_name,
+                description: menuItem.description
+            });
+          }
+        });
+
+        // return processed items
+        return foundItems;
+      });
+    };
+  }
+
+})();
